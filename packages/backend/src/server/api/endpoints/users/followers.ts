3634191fdd3a925612bb60bec74dbf2b45c0a9c3
HanasakiNayori <hibariruri@gmail.com>
2024-08-13 02:54:50 +0900

    FFが非公開の場合外から見えないように - 追加

--- packages/backend/src/server/api/endpoints/users/followers.ts
+++ packages/backend/src/server/api/endpoints/users/followers.ts
@@ -93,25 +93,36 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 
 			const profile = await this.userProfilesRepository.findOneByOrFail({ userId: user.id });
 
-			if (profile.followersVisibility === 'private') {
-				if (me == null || (me.id !== user.id)) {
-					throw new ApiError(meta.errors.forbidden);
-				}
-			} else if (profile.followersVisibility === 'followers') {
-				if (me == null) {
-					throw new ApiError(meta.errors.forbidden);
-				} else if (me.id !== user.id) {
-					const isFollowing = await this.followingsRepository.exists({
-						where: {
-							followeeId: user.id,
-							followerId: me.id,
-						},
-					});
-					if (!isFollowing) {
+			if (profile.followersVisibility !== 'public') {
+				if (profile.followersVisibility === 'private') {
+					if (me == null || (me.id !== user.id)) {
 						throw new ApiError(meta.errors.forbidden);
 					}
+				} else if (profile.followersVisibility === 'followers') {
+					if (me == null) {
+						throw new ApiError(meta.errors.forbidden);
+					} else if (me.id !== user.id) {
+						const isFollowing = await this.followingsRepository.exists({
+							where: {
+								followeeId: user.id,
+								followerId: me.id,
+							},
+						});
+						if (!isFollowing) {
+							throw new ApiError(meta.errors.forbidden);
+						} else if (me.id !== user.id) {
+							const isFollowing = await this.followingsRepository.exists({
+								where: {
+									followeeId: user.id,
+									followerId: me.id,
+								},
+							});
+							if (!isFollowing) {
+								throw new ApiError(meta.errors.forbidden);
+							}
+						}
+					}
 				}
-			}
 
 			const query = this.queryService.makePaginationQuery(this.followingsRepository.createQueryBuilder('following'), ps.sinceId, ps.untilId)
 				.andWhere('following.followeeId = :userId', { userId: user.id })
--- packages/backend/src/server/api/endpoints/users/following.ts
+++ packages/backend/src/server/api/endpoints/users/following.ts
@@ -102,25 +102,36 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 
 			const profile = await this.userProfilesRepository.findOneByOrFail({ userId: user.id });
 
-			if (profile.followingVisibility === 'private') {
-				if (me == null || (me.id !== user.id)) {
-					throw new ApiError(meta.errors.forbidden);
-				}
-			} else if (profile.followingVisibility === 'followers') {
-				if (me == null) {
-					throw new ApiError(meta.errors.forbidden);
-				} else if (me.id !== user.id) {
-					const isFollowing = await this.followingsRepository.exists({
-						where: {
-							followeeId: user.id,
-							followerId: me.id,
-						},
-					});
-					if (!isFollowing) {
+			if (profile.followersVisibility !== 'public') {
+				if (profile.followersVisibility === 'private') {
+					if (me == null || (me.id !== user.id)) {
 						throw new ApiError(meta.errors.forbidden);
 					}
+				} else if (profile.followersVisibility === 'followers') {
+					if (me == null) {
+						throw new ApiError(meta.errors.forbidden);
+					} else if (me.id !== user.id) {
+						const isFollowing = await this.followingsRepository.exists({
+							where: {
+								followeeId: user.id,
+								followerId: me.id,
+							},
+						});
+						if (!isFollowing) {
+							throw new ApiError(meta.errors.forbidden);
+						} else if (me.id !== user.id) {
+							const isFollowing = await this.followingsRepository.exists({
+								where: {
+									followeeId: user.id,
+									followerId: me.id,
+								},
+							});
+							if (!isFollowing) {
+								throw new ApiError(meta.errors.forbidden);
+							}
+						}
+					}
 				}
-			}
 
 			const query = this.queryService.makePaginationQuery(this.followingsRepository.createQueryBuilder('following'), ps.sinceId, ps.untilId)
 				.andWhere('following.followerId = :userId', { userId: user.id })
