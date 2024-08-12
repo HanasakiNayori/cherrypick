37c61d674dd66c5d03fe8406569b035399fc17fc
HanasakiNayori <hibariruri@gmail.com>
2024-08-13 03:09:10 +0900

    FFが非公開の場合外から見えないように - 修正3

--- packages/backend/src/server/api/endpoints/users/followers.ts
+++ packages/backend/src/server/api/endpoints/users/followers.ts
@@ -110,7 +110,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 						});
 						if (!isFollowing) {
 							throw new ApiError(meta.errors.forbidden);
-											} else if (me.id !== user.id) {
+						} else if (me.id !== user.id) {
 						const isFollowing = await this.followingsRepository.exists({
 							where: {
 								followeeId: user.id,
@@ -119,20 +119,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 						});
 						if (!isFollowing) {
 							throw new ApiError(meta.errors.forbidden);
-						} else if (me.id !== user.id) {
-							const isFollowing = await this.followingsRepository.exists({
-								where: {
-									followeeId: user.id,
-									followerId: me.id,
-								},
-							});
-							if (!isFollowing) {
-								throw new ApiError(meta.errors.forbidden);
-							}
 						}
 					}
 				}
 			}
+		}
 
 			const query = this.queryService.makePaginationQuery(this.followingsRepository.createQueryBuilder('following'), ps.sinceId, ps.untilId)
 				.andWhere('following.followeeId = :userId', { userId: user.id })
--- packages/backend/src/server/api/endpoints/users/following.ts
+++ packages/backend/src/server/api/endpoints/users/following.ts
@@ -119,16 +119,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 						});
 						if (!isFollowing) {
 							throw new ApiError(meta.errors.forbidden);
-						} else if (me.id !== user.id) {
-							const isFollowing = await this.followingsRepository.exists({
-								where: {
-									followeeId: user.id,
-									followerId: me.id,
-								},
-							});
-							if (!isFollowing) {
-								throw new ApiError(meta.errors.forbidden);
-							}
 						}
 					}
 				}
