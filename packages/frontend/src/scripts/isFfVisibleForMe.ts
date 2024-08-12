d647b0fb49c68e0a79dde8c99793f812e56e60aa
HanasakiNayori <hibariruri@gmail.com>
2024-08-13 06:38:33 +0900

    戻しました

--- packages/frontend/src/scripts/isFfVisibleForMe.ts
+++ packages/frontend/src/scripts/isFfVisibleForMe.ts
@@ -9,35 +9,16 @@ import { $i } from '@/account.js';
 export function isFollowingVisibleForMe(user: Misskey.entities.UserDetailed): boolean {
 	if ($i && $i.id === user.id) return true;
 
-	// ローカルユーザーの場合
-	if (user.host === null) {
-		if (user.followingVisibility === 'private') return false;
-		if (user.followingVisibility === 'followers' && !user.isFollowing) return false;
-	}
-
-	// リモートユーザーの場合
-	if (user.host !== null) {
-		if (user.followingVisibility === 'private') return false;
-		if (user.followingVisibility === 'followers' && !user.isFollowing) return false;
-	}
+	if (user.followingVisibility === 'private') return false;
+	if (user.followingVisibility === 'followers' && !user.isFollowing) return false;
 
 	return true;
 }
-
 export function isFollowersVisibleForMe(user: Misskey.entities.UserDetailed): boolean {
 	if ($i && $i.id === user.id) return true;
 
-	// ローカルユーザーの場合
-	if (user.host === null) {
-		if (user.followersVisibility === 'private') return false;
-		if (user.followersVisibility === 'followers' && !user.isFollowing) return false;
-	}
-
-	// リモートユーザーの場合
-	if (user.host !== null) {
-		if (user.followersVisibility === 'private') return false;
-		if (user.followersVisibility === 'followers' && !user.isFollowing) return false;
-	}
+	if (user.followersVisibility === 'private') return false;
+	if (user.followersVisibility === 'followers' && !user.isFollowing) return false;
 
 	return true;
 }
